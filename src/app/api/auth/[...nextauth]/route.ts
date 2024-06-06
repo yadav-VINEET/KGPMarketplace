81aa3f68b56fb736978650726dc1a3345ff744fa
Vineet Yadav <98595275+yadav-VINEET@users.noreply.github.com>
2024-06-06 15:31:43 +0530

    route correction

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,19 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-if(!process.env.GOOGLE_CLIENT_SECRET){
-    throw 'google client id is missing';
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error('Google client secret is missing');
 }
 
 export const authOptions = {
-    secret : process.env.SECRET,
-    providers : [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-    ],
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
 };
+
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
